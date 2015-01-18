fc152da516e4a700a4826b34461d2a4769d2d0bc
qaz-s <kazuki-sato@kazuki-sato.local>
2015-01-18 19:02:39 +0900

    インデックス数が1件の時に、処理止まる不具合修正

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var async = require('async');				// 同期処理
 async.eachSeries(config.data, function(data, next) {
 	var query = "site:" + data.url;
 	client.fetch(config.url, { q: query }, function (err, $, res) {
-		var index = $("#resultStats").text().split("約 ")[1].split(" 件")[0];
+		var index = getIndexNumber($("#resultStats").text());
 		var text = data.title + " : " + index + "\n";
 		console.log(data.title + " : " + index);
 		fs.appendFile('result.txt', text ,'utf8', function (err) {});
@@ -16,3 +16,14 @@ async.eachSeries(config.data, function(data, next) {
 }, function(err) {
     console.log('all done!');
 });
+
+
+function getIndexNumber(result) {
+	var number;
+	if (result.indexOf("約") !== -1) {
+		number = result.split("約 ")[1].split(" 件")[0].replace(",", "");
+	} else {
+		number = result.split(" 件")[0];
+	}
+	return number;
+}
