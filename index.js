c4539fe09a51f6f6e1c827e9021da49286ca14fb
qaz-s <kazuki-sato@kazuki-sato.local>
2015-06-20 19:15:49 +0900

    cron実行するよう処理修正

--- index.js
+++ index.js
@@ -3,19 +3,38 @@ var config = require('config');				// 定義
 var client = require('cheerio-httpcli');	// 通信
 var fs = require('fs');						// ファイル
 var async = require('async');				// 同期処理
+var cronJob = require('cron').CronJob;		// cron
+var cronTime = "0 0 0 * * 0-6";				// cron実行時刻。毎日0:00:00に実行
 
-async.eachSeries(config.data, function(data, next) {
-	var query = "site:" + data.url;
-	client.fetch(config.url, { q: query }, function (err, $, res) {
-		var index = getIndexNumber($("#resultStats").text());
-		var text = data.title + " : " + index + "\n";
-		console.log(data.title + " : " + index);
-		fs.appendFile('result.txt', text ,'utf8', function (err) {});
-		next();
-	});
-}, function(err) {
-    console.log('all done!');
+var job = new cronJob({
+	//実行したい日時 or crontab書式
+	cronTime: cronTime,
+	//指定時に実行したい関数
+	onTick: function() {
+		// インデックス集計処理
+		async.eachSeries(config.data, function(data, next) {
+			var query = "site:" + data.url;
+			client.fetch(config.url, { q: query }, function (err, $, res) {
+				var index = getIndexNumber($("#resultStats").text());
+				var text = data.title + " : " + index + "\n";
+				console.log(data.title + " : " + index);
+				fs.appendFile('result.csv', text ,'utf8', function (err) {});
+				next();
+			});
+		}, function(err) {
+		    console.log('all done!');
+		});
+	},
+	//ジョブの完了または停止時に実行する関数 
+	onComplete: function() {
+		console.log('onComplete!');
+	},
+	// コンストラクタを終する前にジョブを開始するかどうか
+	start: false,
+	//タイムゾーン
+	timeZone: "Asia/Tokyo"
 });
+job.start();	//ジョブ開始
 
 
 function getIndexNumber(result) {
