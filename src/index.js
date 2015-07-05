64d4f040ac568d627cbe6b01937df8a3ef9106bd
qaz-s <qaz.s.co@gmail.com>
2015-07-05 23:28:19 +0900

    cron実行機能切り替え可能なように設定を追加

--- src/index.js
+++ src/index.js
@@ -1,48 +1,69 @@
 // モジュール読み込み
-var config = require('config');        // 定義
-var client = require('cheerio-httpcli');  // 通信
-var fs = require('fs');            // ファイル
-var async = require('async');        // 同期処理
-var cronJob = require('cron').CronJob;    // cron
-var cronTime = "0 0 0 * * 0-6";        // cron実行時刻。毎日0:00:00に実行
+var config      = require('config');                  // 定義
+var cronJob     = require('cron').CronJob;            // cron
+var client      = require('cheerio-httpcli');         // 通信
+var fs          = require('fs');                      // ファイル
+var async       = require('async');                   // 同期処理
+var Spreadsheet = require('edit-google-spreadsheet'); // スプレッドシート
 
-var job = new cronJob({
-  //実行したい日時 or crontab書式
-  cronTime: cronTime,
-  //指定時に実行したい関数
-  onTick: function() {
-    // インデックス集計処理
-    async.eachSeries(config.data, function(data, next) {
-      var query = "site:" + data.url;
-      client.fetch(config.url, { q: query }, function (err, $, res) {
-        var index = getIndexNumber($("#resultStats").text());
-        var text = data.title + " : " + index + "\n";
-        console.log(data.title + " : " + index);
-        fs.appendFile('result.csv', text ,'utf8', function (err) {});
-        next();
-      });
-    }, function(err) {
-        console.log('all done!');
+var start = function () {
+  cronFunc(function () {
+    fetchFunc(function (indexes) {
+      spreadSheetFunc(indexes);
+      csvFileFunc(indexes);
     });
-  },
-  //ジョブの完了または停止時に実行する関数
-  onComplete: function() {
-    console.log('onComplete!');
-  },
-  // コンストラクタを終する前にジョブを開始するかどうか
-  start: false,
-  //タイムゾーン
-  timeZone: "Asia/Tokyo"
-});
-job.start();  //ジョブ開始
+  });
+};
 
-
-function getIndexNumber(result) {
-  var number;
-  if (result.indexOf("約") !== -1) {
-    number = result.split("約 ")[1].split(" 件")[0].replace(",", "");
+var cronFunc = function (callback) {
+  if (config.global.cron.enable) {
+    var job = new cronJob({
+      cronTime: config.global.cron.time,  //実行したい日時 or crontab書式
+      onTick: function() {  //指定時に実行したい関数
+        callback();
+      },
+      onComplete: function() {  //ジョブの完了または停止時に実行する関数
+      },
+      start: false, // コンストラクタを終する前にジョブを開始するかどうか
+      timeZone: "Asia/Tokyo"  //タイムゾーン
+    });
+    job.start();  //ジョブ開始
   } else {
-    number = result.split(" 件")[0];
+    callback();
   }
-  return number;
-}
+};
+
+var fetchFunc = function (callback) {
+  var indexes = [];
+  async.eachSeries(config.data, function(data, next) {
+    var query = "site:" + data.url;
+    client.fetch(config.global.url, { q: query }, function (err, $, res) {
+      // HTML解析
+      $("#resultStats").find("nobr").remove();
+      var index = {
+        title: data.title,
+        number: $("#resultStats").text().replace(/,/g, "").match(/\d+/g)[0]
+      };
+      console.log(index.title + " : " + index.number);
+      indexes.push(index);
+      next();
+    });
+  }, function(err) {
+      callback(indexes);
+  });
+};
+
+var spreadSheetFunc = function (indexes) {
+  if (!config.global.spreadsheet.enable) return;
+};
+
+var csvFileFunc = function (indexes) {
+  if (!config.global.csv.enable) return;
+  var text = "";
+  for (var i = 0; i < indexes.length; i++) {
+    text += indexes[i].title + "," + indexes[i].number + "\n";
+  }
+  fs.appendFile('result.csv',text ,'utf8', function (err) {});
+};
+
+start();  // 実行
